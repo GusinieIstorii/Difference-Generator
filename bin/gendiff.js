80f1e1a562fafd7a75b7fb0a59ae125077f61242
Kseniia <ksenia.guseva789@gmail.com>
2023-05-10 14:26:41 +0400

    update gendiff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,5 +6,6 @@ const program = new Command();
 
 program
   .description('Compares two configuration files and shows a difference.')
-  .version('0.0.1', '-V, --version', 'output the version number')
-  .helpOption('-h, --HELP', 'output usage information');
+  .version('0.0.1', '-V, --version', 'output the version number');
+
+program.parse();
