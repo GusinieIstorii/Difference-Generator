47053c2277fc53bcf4263c9276c42d326016fd26
Kseniia <ksenia.guseva789@gmail.com>
2023-05-10 16:51:16 +0400

    modified gendiff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,8 +4,14 @@ import { Command } from 'commander';
 
 const program = new Command();
 
+// const format = (type) => console.log(type);
+
 program
   .description('Compares two configuration files and shows a difference.')
-  .version('0.0.1', '-V, --version', 'output the version number');
+  .version('0.0.1', '-V, --version', 'output the version number')
+  // .action(format)
+  .helpOption('-h, --help', 'output usage information')
+  .option('-f, --format, <type>', 'output format')
+  .arguments('<file1> <file2>');
 
 program.parse();
